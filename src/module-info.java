cb457f7364e37bd2272122f94f7228e00ba87f36
Nullpointer <nullpointerworks@outlook.com>
2019-07-08 15:14:08 +0200

    module compatibility

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,12 @@
-module libcore
+/**
+ * @since 1.0
+ */
+module com.nullpointerworks.core
 {
 	requires java.base;
 	requires transitive java.desktop;
 	exports com.nullpointerworks.core;
+	exports com.nullpointerworks.core.window;
 	exports com.nullpointerworks.core.buffer;
 	exports com.nullpointerworks.core.buffer.concurrency;
 	exports com.nullpointerworks.core.input;
